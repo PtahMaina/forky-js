81b061fea7070aa0516344023280c4d757ad562e
PtahMaina <ptahmwass@gmail.com>
2022-02-15 15:56:00 +0300

    Error correction

--- starter/src/js/views/paginationView.js
+++ starter/src/js/views/paginationView.js
@@ -2,69 +2,74 @@ import View from './View';
 import icons from 'url:../../img/icons.svg';
 class paginationView extends View {
   _parentElement = document.querySelector('.pagination');
+
   addHandlerClick(handler) {
     this._parentElement.addEventListener('click', function (e) {
       const btn = e.target.closest('.btn--inline');
       if (!btn) return;
 
-      const goToPage = btn.dataSet.goto;
-      console.log(goToPage);
-
+      const goToPage = +btn.dataset.goto;
       handler(goToPage);
     });
   }
+
   _generateMarkup() {
     const curPage = this._data.page;
-    const numPage = math.ceil(
+    const numPages = Math.ceil(
       this._data.results.length / this._data.resultsPerPage
     );
-    console.log(numPage);
-    //page 1 and there are other pages
-    if (curPage === 1 && numPage > 1) {
+
+    // Page 1, and there are other pages
+    if (curPage === 1 && numPages > 1) {
       return `
-      <button go-to="${curPage + 1}" class="btn--inline pagination__btn--next">
-            <span>Page ${curPage + 1}</span>
-            <svg class="search__icon">
-              <use href="${icons}#icon-arrow-right"></use>
-            </svg>
-          </button>
-      `;
-    }
-    //last page
-    if (curPage === numPage && numPage > 1) {
-      return `
-        <button gol-to="${
-          curPage - 1
-        }" class="btn--inline pagination__btn--prev">
-            <svg class="search__icon">
-              <use href="${icons}#icon-arrow-left"></use>
-            </svg>
-            <span>Page ${curPage - 1}</span>
-        </button>
-      `;
-    }
-    //other page
-    if (curPage < numPage) {
-      return `
-        <button gol-to="${
-          curPage - 1
-        }" class="btn--inline pagination__btn--prev">
-            <svg class="search__icon">
-              <use href="${icons}#icon-arrow-left"></use>
-            </svg>
-            <span>Page ${curPage - 1}</span>
-        </button>
-        <button gol-to="${
+        <button data-goto="${
           curPage + 1
         }" class="btn--inline pagination__btn--next">
-            <span>Page ${curPage + 1}</span>
-            <svg class="search__icon">
-              <use href="${icons}#icon-arrow-right"></use>
-            </svg>
+          <span>Page ${curPage + 1}</span>
+          <svg class="search__icon">
+            <use href="${icons}#icon-arrow-right"></use>
+          </svg>
         </button>
       `;
     }
-    //page 1 and there are no other pages
+
+    // Last page
+    if (curPage === numPages && numPages > 1) {
+      return `
+        <button data-goto="${
+          curPage - 1
+        }" class="btn--inline pagination__btn--prev">
+          <svg class="search__icon">
+            <use href="${icons}#icon-arrow-left"></use>
+          </svg>
+          <span>Page ${curPage - 1}</span>
+        </button>
+      `;
+    }
+
+    // Other page
+    if (curPage < numPages) {
+      return `
+        <button data-goto="${
+          curPage - 1
+        }" class="btn--inline pagination__btn--prev">
+          <svg class="search__icon">
+            <use href="${icons}#icon-arrow-left"></use>
+          </svg>
+          <span>Page ${curPage - 1}</span>
+        </button>
+        <button data-goto="${
+          curPage + 1
+        }" class="btn--inline pagination__btn--next">
+          <span>Page ${curPage + 1}</span>
+          <svg class="search__icon">
+            <use href="${icons}#icon-arrow-right"></use>
+          </svg>
+        </button>
+      `;
+    }
+
+    // Page 1, and there are NO other pages
     return '';
   }
 }
